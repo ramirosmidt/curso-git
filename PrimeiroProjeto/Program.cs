361e02435886f5ba36ea9745e6d7eef2bca8c5b5
Ramirosmidt <ramirosmidt0@gmail.com>
2020-12-10 01:23:56 -0300

    Projeto iniciado, mostrando ola mundo

---
+++ PrimeiroProjeto/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PrimeiroProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá Mundo");
+            Console.ReadLine();
+        }
+    }
+}
